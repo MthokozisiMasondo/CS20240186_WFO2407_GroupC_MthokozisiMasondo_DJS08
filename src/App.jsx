9bf56ebd14c02f18f1b78d80f5e7411375fb8aee
Mthokozisi Masondo <m.masondo2001@gmail.com>
2024-11-16 17:57:50 +0200

    organized pages into a file

--- src/App.jsx
+++ src/App.jsx
@@ -15,34 +15,10 @@
  */
 
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 import "./server"
 
-function Home() {
-  return (
-    <div className='home-container'>
-      <h1>You got the travel plans, we got the travel vans.</h1>
-      <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-      <Link to="vans">Find your van</Link>
-    </div>
-  )
-}
-
-function About() {
-  return (
-    <div className=''>
-      <img className='about-hero-image' src="" alt="" />
-      <div className='about-page-content'>
-        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
-        <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉)</p>
-        <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
-      </div>
-      <div className='about-page-cta'>
-        <h2>Your destination is waiting.<br />Your van is ready.</h2>
-        <Link className="link-button" to="/vans">Explore our vans</Link>
-      </div>
-    </div>
-  )
-}
 
 function App() {
 
